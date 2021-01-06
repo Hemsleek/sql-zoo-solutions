c394b9452d521101be6ec3318101e7e31f5033ce
hemsleek <asiyanbimubashir@gmail.com>
2021-01-06 23:01:24 +0100

    feat: sql select quiz

--- 1_sql_select_from_world.sql
+++ 1_sql_select_from_world.sql
@@ -25,4 +25,14 @@
     -- 7. Show the countries that are big by area or big by population. Show name, population and area.
 
     SELECT name,population,area FROM world WHERE area >3000000 OR population>250000000
+    -- 8. Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+
+    SELECT name,population,area FROM world WHERE area > 3000000 XOR population > 250000000
+
+    -- 9. For South America show population in millions and GDP in billions both to 2 decimal places.
+    SELECT name, ROUND(population / 1000000 , 2), ROUND(gdp / 1000000000 , 2) FROM world WHERE continent ='South America'
+
+    -- 10. Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+    SELECT name,ROUND(gdp /population,-3 ) FROM world WHERE gdp > 1000000000000
+
     
