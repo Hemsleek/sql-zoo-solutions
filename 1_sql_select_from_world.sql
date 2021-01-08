2cc833b2a315c1bee650c403e14b0bf9a553f277
hemsleek <asiyanbimubashir@gmail.com>
2021-01-08 22:52:35 +0100

    feat: sql select world quiz

--- 1_sql_select_from_world.sql
+++ 1_sql_select_from_world.sql
@@ -35,4 +35,16 @@
     -- 10. Show per-capita GDP for the trillion dollar countries to the nearest $1000.
     SELECT name,ROUND(gdp /population,-3 ) FROM world WHERE gdp > 1000000000000
 
-    
+    -- 11. Show the name and capital where the name and the capital have the same number of characters.
+    SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital);
+
+    -- 12. Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+        You can use the function LEFT to isolate the first character.
+        You can use <> as the NOT EQUALS operator.
+
+    SELECT name,capital FROM world WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital
+
+    -- 13. Find the country that has all the vowels and no spaces in its name 
+    SELECT name FROM world
+    WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' 
+    AND name NOT LIKE '% %' 
